3c59408afa9cb3d903c2dd60c29eb5d1bf5ad5f5
Shivi110121 <77702799+Shivi110121@users.noreply.github.com>
2022-02-09 12:06:07 -0500

    Add files via upload

---
+++ KillerWhale.java
@@ -0,0 +1,14 @@
+package csvTableProject;
+
+class KillerWhale
+{
+	KillerWhale()
+	{
+		System.out.println("Killer Whale Class Working");
+	}
+	
+	void KillerWhaleMethod() 
+	{
+		System.out.println("What does this void method do?");
+	}
+}
